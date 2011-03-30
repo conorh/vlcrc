eb94af1e61f442ad4ea62f32effd40b157b3ebde
Carl Suster <carl.suster@gmail.com>
2011-03-30 20:45:57 +1100

    version bump

--- lib/vlcrc/version.rb
+++ lib/vlcrc/version.rb
@@ -3,5 +3,5 @@ module VLCRC
   # about the git repository. The Major and Minor bits are from the
   # name of the nearest tag, and the third bit is the number of commits
   # since the tag
-  VERSION = "0.0.1"
+  VERSION = "0.1.1"
 end
