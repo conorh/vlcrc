c80f8a04f116996b3a29ef03c97f5752980506cb
Carl Suster <carl.suster@gmail.com>
2011-04-30 20:49:05 +1000

    switch to use ms not second and neatened comments

--- lib/vlcrc/core.rb
+++ lib/vlcrc/core.rb
@@ -4,13 +4,13 @@ require 'timeout'
 module VLCRC
 
   # Number of seconds to wait for a response before deciding that there
-  # is no message waiting in the socket
+  # is no message waiting in the socket.
   WAIT = 0.2
 
-  # Bindings for accessing VLC media player over a TCP socket
+  # Bindings for accessing VLC media player over a TCP socket.
   class VLC
 
-    # Attempt to connect to the given TCP socket, return new VLC object
+    # Attempt to connect to the given TCP socket, return new VLC object.
     def initialize( host='localhost', port=1234 )
       @host = 'localhost'
       @port = port
@@ -18,7 +18,8 @@ module VLCRC
     end
 
     # Connect to the TCP socket associated with this VLC object and store it
-    # in the @socket instance variable, or nil if there is a problem connecting
+    # in the @socket instance variable, or nil if there is a problem
+    # connecting.
     def connect
       begin
         @socket = TCPSocket.new @host, @port
@@ -31,11 +32,12 @@ module VLCRC
     end
 
     # Launch an instance of VLC with the RC interface configured for the
-    # specified TCP socket unless there already is one
+    # specified TCP socket unless there already is one.
     def launch
       return false if connected?
       %x{ vlc --lua-config "rc={host='#{@host}:#{@port}',prompt=''}" >/dev/null 2>&1 & }
       # TODO windows version: `xxx &` ~~> `start xxx`
+      # TODO windows version: /dev/null ~~> NUL
       # TODO pre-lua rc interface (VLC version detection?)
       true
     end
@@ -55,17 +57,17 @@ module VLCRC
       true
     end
 
-    # Get current playing state (i.e. playing or stopped)
+    # Get current playing state (i.e. playing or stopped).
     def playing
       on = ask "is_playing"
       return false unless on
       [false, true][on.to_i]
     end
 
-    # Toggle pause
+    # Toggle pause.
     def pause() ask "pause", false end
 
-    # Set current playing state
+    # Set current playing state.
     def playing=( play )
       if play
         ask "play", false
@@ -74,22 +76,22 @@ module VLCRC
       end
     end
 
-    # Get current position in the file (in seconds)
-    def position() ask( "get_time" ).to_i end
+    # Get current position in the file (in ms).
+    def position() ask( "get_time" ).to_i*1000 end
 
-    # Seek to a given time in the file (in seconds)
-    def position=( time ) ask "seek #{time}", false end
+    # Seek to a given time in the file (in ms).
+    def position=( time ) ask "seek #{time/1000}", false end
 
-    # Get the total length of the current media (in seconds)
-    def length() ask( "get_length" ).to_i end
+    # Get the total length of the current media (in ms).
+    def length() ask( "get_length" ).to_i*1000 end
 
-    # Close the bound instance of the media player
+    # Close the bound instance of the media player.
     def exit() ask "shutdown" end
 
-    # Close the current TCP connection
+    # Close the current TCP connection.
     def disconnect() @socket, @vlc_version = nil, nil if ask "quit" end
 
-    # Get the currently playing media
+    # Get the currently playing media.
     def media()
       if playing
         status = long_ask "status"
@@ -102,29 +104,29 @@ module VLCRC
       end
     end
 
-    # Open a given file in the current media player instance
+    # Open a given file in the current media player instance.
     def media=( file ) ask "add file://#{file}", false end
 
-    # Get the currently selected subtitle track
+    # Get the currently selected subtitle track.
     def subtitle() ask( "strack" ).to_i end
 
-    # Set the subtitle track
+    # Set the subtitle track.
     def subtitle=( track ) ask "strack #{track}", false end
 
-    # Get the framerate
+    # Get the framerate.
     def fps
       info = long_ask "info"
       return false unless info
       info.scan( /Frame rate: (\d*)/ )[0][0].to_i
     end
 
-    # Skip to the next item in the playlist
+    # Skip to the next item in the playlist.
     def next() ask "next", false end
 
-    # Go back to previous item in the playlist
+    # Go back to previous item in the playlist.
     def prev() ask "prev", false end
 
-    # Look at the contents of the playlist
+    # Look at the contents of the playlist.
     def playlist
       raw = long_ask "playlist"
       playlist_id = raw.scan( /\| (\d*) - Playlist/ )[0][0]
@@ -137,7 +139,7 @@ module VLCRC
       end
     end
 
-    # Set the contents of the playlist
+    # Set the contents of the playlist.
     def playlist=( queue )
       ask "clear", false
       queue.each do |file|
@@ -147,22 +149,20 @@ module VLCRC
 
     private
 
-    # Empty out the socket of any waiting messages and return them
+    # Empty out the socket of any waiting messages and return them.
     def clear
       msg = ''
       return nil unless connected?
       begin
         loop do
-          timeout( WAIT ) do
-            msg += @socket.gets
-          end
+          timeout( WAIT ) { msg += @socket.gets }
         end
       rescue Timeout::Error
         return msg
       end
     end
 
-    # Read off one line from the socket, error on timeout
+    # Read off one line from the socket, error on timeout.
     def gets
       return false unless connected?
       begin
@@ -175,7 +175,8 @@ module VLCRC
     end
 
     # Send a message to the socket after first clearing anything in it, and
-    # return the first line of the response unless the second argument is false
+    # return the first line of the response unless the second argument is
+    # false.
     def ask( str, returns=true )
       return false unless connected?
       clear
@@ -183,7 +184,7 @@ module VLCRC
       returns ? gets : true
     end
 
-    # Send a message to the socket and return the full response
+    # Send a message to the socket and return the full response.
     def long_ask( str )
       return false unless connected?
       clear
